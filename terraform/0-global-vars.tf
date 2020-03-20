bcd34ca1e258be0625dabd6f52f59553e8ccce57
David Mauricio Gutierrez Ruiz <david.gutierrez@satrack.com>
2020-03-20 10:05:02 -0500

    Ccambios

---
+++ terraform/0-global-vars.tf
@@ -0,0 +1,20 @@
+variable "client_id" {
+  type    = string
+  default = "ce63f12f-f0de-4fbe-abef-1b8ad05d8b2e"
+}
+
+variable "client_secret" {
+  type    = string
+  default = "e9a5e5ce-01f8-4ab1-adfa-74909463a6b3"
+}
+
+variable "subscription_id" {
+  type    = string
+  default = "2c1c3374-9ff5-4aff-9586-b5366e44a8b0"
+}
+
+variable "tenant_id" {
+  type    = string
+  default = "38a36b64-deb1-4f65-936f-37bcef770d53"
+}
+
---
+++ terraform/2-resources-variables.tf
@@ -0,0 +1,36 @@
+variable "resource_group_name" {
+  default = "GRPHdInsight2"
+}
+
+variable "location" {
+  default = "East US 2"
+}
+
+variable "storage_account_name" {
+  default = "stohdinsightdev"
+}
+
+variable "storage_container_name" {
+  default = "hdinsight"
+}
+
+variable "hbase_cluster_name" {
+  default = "hbaseinsight01"
+}
+
+variable "ambari_user_name" {
+  default = "admin"
+}
+
+variable "ambari_password" {
+  default = "TerrAform123!"
+}
+
+variable "ssh_user_name" {
+  default = "sshuser"
+}
+
+variable "ssh_password" {
+  default = "AccTestvdSC4daf986!"
+}
+
